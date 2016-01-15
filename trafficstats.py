f5cb199f5d7f660da1b4d91a0196a70df73f216f
jian <j60017268@gmail.com>
2016-01-15 11:07:28 +0800

    first realease

---
+++ trafficstats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import paramiko
+import threading
+
+def ssh2(ip,username,passwd):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip,22,username,passwd,timeout=5)
+        stdin, stdout, stderr = ssh.exec_command('cat /proc/net/dev')
+        out = stdout.readlines()
+        conn = out[2].split()
+        ReceiveBytes = round(float(conn[1])/(10**9),1)
+        TransmitBytes = round(float(conn[9])/(10**9),1)
+        print '%s\t%s\t%s'%(ip,ReceiveBytes,TransmitBytes)
+        ssh.close()
+    except :
+        print '%s\tError'%(ip)
+
+if __name__=='__main__':
+    ip_list=['192.168.1.55','192.168.1.100','192.168.1.101','192.168.1.200','192.168.1.201']
+    username = "root"  #用户名
+    passwd = ""    #密码
+    threads = []   #多线程
+    print '\nIP\t\tRX(G)\tTX(G)'
+    for ip in ip_list:
+        ssh2(ip,username,passwd)
+    print '\n'
